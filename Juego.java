37724d6ae9da4bf09a326af3b1b28d4696320e9e
Javier Calvo <javiercalvoporro@gmail.com>
2023-12-12 20:13:17 +0100

    Falta generar la Matriz con el String nuevo

--- Juego.java
+++ Juego.java
@@ -3,6 +3,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.text.DecimalFormat;
+import java.util.Scanner;
+
+//Para detectar el Enter
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Juego{
     public static void main(String[] args) {
@@ -17,7 +23,7 @@ public class Juego{
         }
 
         //Se comenzara con el primer tablero
-        int tablero = 1;
+        int tablero = 0;
         String archivo = "tableros.txt";
         //Aqui se guardara el tablero que se va a jugar
         String linea = null;
@@ -28,7 +34,7 @@ public class Juego{
         while (true) {
 
             try{
-                int contador = 1;
+                int contador = 0;
                 BufferedReader br = new BufferedReader(new FileReader(archivo));
                 while (contador <= tablero) {
                     linea = br.readLine();
@@ -66,9 +72,7 @@ public class Juego{
 
             else{
                 //Hay tableros donde jugar
-                //StringBuilder donde se guardaran las casillas fijas donde no se podran mover
                 StringBuilder casillaBloq = new StringBuilder();
-                System.out.println(linea);
                 int indiceTablero = 0;
             
                 for (int fila = 1; fila < matriz.length; fila++) {
@@ -122,12 +126,100 @@ public class Juego{
                     System.out.println();
                 }
                 //Ahora generamos el array para compararlo posteriormente
-                String[] arrayCasillasBlocked = casillaBloq.toString().split(" ");
-                break;
-                //Aqui ya empezaria a pedir un Scanner con el que introduzca el usuario algo y lo comparase si son fijas o no, si quedan casillas por resolver... etc
-            }
 
-        }
+                String[] arrayCasillasBlocked = casillaBloq.toString().split(" ");
+                String lineaModificable = linea.replaceAll(" ", "");
+
+                while (true) {
+                    //Recorremos la Matriz para ver si esta vacía o no
+                    int count = 0;
+                    for (int fila = 1; fila < matriz.length; fila++) {
+                        for (int col = 1; col < matriz[fila].length; col++) {
+                            if (matriz[fila][col] == 'x' || matriz[fila][col] == 'o') {
+                                count++;
+                            } 
+                        }
+                    }
+                    //Si es 36, esta llena
+                    Scanner in = new Scanner(System.in); 
+                    if (count == 36){
+                        System.out.println("Pulse tecla intro si ha terminado:");
+                        System.out.print("Jugada:" + "\t");
+                    }
+                    else{
+                        //Comprobar si la entrada es correcta
+                        System.out.print("Jugada: ");
+                    }
+
+                    String casilla = in.nextLine();
+                        if (casilla.length() == 2) {
+                            char primerCaracter = casilla.charAt(0);
+                            char segundoCaracter = casilla.charAt(1);
+                            if (primerCaracter >= '1' && primerCaracter <= '6' &&
+                            segundoCaracter >= 'A' && segundoCaracter <= 'F') {
+                                //Comprobar si la casilla está bloqueada
+                                int cent = 0;
+                                for (int i=0; i<arrayCasillasBlocked.length; i++){
+
+                                    if (arrayCasillasBlocked[i].equals(casilla)){
+                                        cent++;
+                                    }
+                                }
+
+                                if (cent == 1){
+                                   System.out.println("Esta casilla está bloqueada."); 
+                                }
+
+                                else{
+                                    //La jugada puede proseguir, ya que no está bloqueada.
+                                    //Hay que saber la posicion de la jugada en el Array y cambiarla.
+                                    int v1 = (Character.getNumericValue(casilla.charAt(0))-1) * 6;
+                                    int v2;
+                                    switch (casilla.charAt(1)) {
+                                        case 'A':
+                                            v2 = 1; break;
+                                        case 'B':
+                                            v2 = 2; break;
+                                        case 'C':
+                                            v2 = 3; break;
+                                        case 'D':
+                                            v2 = 4; break;
+                                        case 'E':
+                                            v2 = 5; break;    
+                                        default:
+                                            v2 = 6; break;  
+                                    }
+                                    int v = v1 + v2 -1;
+                                    char valorCasilla = lineaModificable.charAt(v);
+                                    StringBuilder build = new StringBuilder(lineaModificable);
+                                    if (valorCasilla == '0' || valorCasilla == '1') {
+                                        build.setCharAt(v, '2');
+                                    } 
+                                    else {
+                                        build.setCharAt(v, '1');
+                                    }
+                                    lineaModificable = build.toString();
+                                    System.out.println(lineaModificable);
+                                }
+                                //Generar la Matriz
+                                
+                            }
+
+                            else{
+                                System.out.println("La entrada no es válida");
+                            }
+                        }
+
+                        else{
+                            System.out.println("La entrada no es válida");
+                        }
+                }
+                
+
+            }
+            
+
+        }       
         
     }
         
